7e83acb185b639d4ef1f8d5ea43ad931127ae8a8
a-kossakowski <106906720+a-kossakowski@users.noreply.github.com>
2024-07-02 12:17:42 +0200

    misc - test_scripts.sh

--- prot_seq_analyzer/test_scripts.sh
+++ prot_seq_analyzer/test_scripts.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # examples for testing purposes
+
 echo "Test for seq_stats.sh:"
 bash ./seq_stats.sh protein_sequences.fasta
 echo "----------------------------------------"
 echo "Test for find_motifs.sh:"
 bash ./find_motifs.sh protein_sequences.fasta GLKNVF
 echo "----------------------------------------"
-echo "Test for eq_manipulate.sh:"
+echo "Test for seq_manipulate.sh:"
 bash ./seq_manipulate.sh protein_sequences.fasta to_low
-
+echo "----------------------------------------"
